4c44ed623a474701ed45277818ab108d91d6200c
mike <mikehike123@gmail.com>
2019-04-07 20:25:45 -0700

    first commit

---
+++ list/add.php
@@ -0,0 +1,67 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+// get database connection
+include_once '../config/database.php';
+ 
+// instantiate product object
+include_once '../objects/listItem.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$listItem = new ListItem($db);
+ 
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// make sure data is not empty
+if(
+    !empty($data->listID) &&
+    !empty($data->userID) &&
+    !empty($data->description) 
+){
+ 
+    // set list property values
+    $listItem->listID = $data->listID;
+    $listItem->userID = $data->userID;
+    $listItem->description = $data->description;
+    $listItem->created = date('Y-m-d H:i:s');
+ 
+    // create the product
+    if($listItem->create()){
+ 
+        // set response code - 201 created
+        http_response_code(201);
+ 
+        // tell the user
+        echo json_encode(array("message" => "List item was created."));
+    }
+ 
+    // if unable to create the product, tell the user
+    else{
+ 
+        // set response code - 503 service unavailable
+        http_response_code(503);
+ 
+        // tell the user
+        echo json_encode(array("message" => "Unable to create list item."));
+    }
+}
+ 
+// tell the user data is incomplete
+else{
+ 
+    // set response code - 400 bad request
+    http_response_code(400);
+ 
+    // tell the user
+    //echo json_encode(array("message" => $data));
+    echo json_encode(array("message" => "Unable to create list item. Data is incomplete."));
+}
+?>
---
+++ objects/listItem.php
@@ -0,0 +1,113 @@
+<?php
+
+class ListItem{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "List";
+ 
+    // object properties
+    public $id;
+    public $listID;
+    public $userID;
+    public $description;
+    public $created;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    // read products
+    function read(){
+ 
+        // select all query
+        $query = "SELECT * FROM " . $this->table_name;
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+    
+    // create list item
+    function create(){
+
+        // query to insert record
+        $query = "INSERT INTO " . $this->table_name . "
+            SET
+                userID=:userID, listID=:listID, description=:description, created=:created";
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->userID=htmlspecialchars(strip_tags($this->userID));
+        $this->listID=htmlspecialchars(strip_tags($this->listID));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->created=htmlspecialchars(strip_tags($this->created));
+
+        // bind values
+        $stmt->bindParam(":userID", $this->userID);
+        $stmt->bindParam(":listID", $this->listID);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":created", $this->created);
+
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+
+        return false;
+
+    }
+    // delete the product
+    function delete(){
+ 
+        // delete query
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // bind id of record to delete
+        $stmt->bindParam(1, $this->id);
+
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+
+        return false;
+     
+    }
+    // update the listItem to completed
+    function completed(){
+ 
+        // delete query
+        $query = "Update " . $this->table_name . " SET completed=:completed WHERE id = :id";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->completed = htmlspecialchars(strip_tags($this->completed));
+
+        // bind id of record to delete
+        $stmt->bindParam(":id", $this->id);
+        $stmt->bindParam(":completed", $this->completed);
+
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+
+        return false;
+     
+    }
+}
