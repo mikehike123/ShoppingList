f2fa25902dc5740ef7abe5f6d89f5baede8452fe
Mike <mikehike123@gmail.com>
2019-04-09 20:07:51 -0700

    headers

--- list/completed.php
+++ list/completed.php
@@ -3,8 +3,10 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
+header("Access-Control-Max-Age: 86400");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
  
 // get database connection
 include_once '../config/database.php';
